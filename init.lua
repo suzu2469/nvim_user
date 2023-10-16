77baf7cbdb9f9627086438d9441ec2651a19a1e2
Souma Suzuki <subayai.kobashiri@gmail.com>
2023-10-16 13:14:06 +0900

    fix

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
-vim.wo.relativenumber = false
-
 return {
   -- Configure AstroNvim updates
   updater = {
@@ -71,6 +69,7 @@ return {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
+    vim.wo.relativenumber = false
     -- Set up custom filetypes
     -- vim.filetype.add {
     --   extension = {
